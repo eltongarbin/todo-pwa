a9dfcc7d31593a7674e7c327a858e37c7f7df9ce
Elton Garbin <eltongarbin@gmail.com>
2019-01-01 22:46:43 -0200

    cache third party resources from a cdn in a react pwa

--- src/sw.js
+++ src/sw.js
@@ -1,17 +1,22 @@
 workbox.skipWaiting();
 workbox.clientsClaim();
 
-self.addEventListener('install', (event) => {
-  const asyncInstall = new Promise((resolve) => {
-    console.log('Waiting to resolve...');
-    setTimeout(resolve, 5000);
-  });
+// self.addEventListener('install', (event) => {
+//   const asyncInstall = new Promise((resolve) => {
+//     console.log('Waiting to resolve...');
+//     setTimeout(resolve, 5000);
+//   });
 
-  event.waitUntil(asyncInstall);
-});
+//   event.waitUntil(asyncInstall);
+// });
 
-self.addEventListener('activate', (event) => {
-  console.log('activated');
-});
+// self.addEventListener('activate', (event) => {
+//   console.log('activated');
+// });
+
+workbox.routing.registerRoute(
+  new RegExp('https:.*min.(css|js)'),
+  workbox.strategies.staleWhileRevalidate({ cacheName: 'cdn-cache' })
+);
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
