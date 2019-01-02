8f5cbea9c454b1115e4b9dd821a48f9f3de8650c
Elton Garbin <eltongarbin@gmail.com>
2019-01-01 22:55:23 -0200

    cache json data in a react pwa with workbox and display it while offline

--- src/sw.js
+++ src/sw.js
@@ -15,8 +15,13 @@ workbox.clientsClaim();
 // });
 
 workbox.routing.registerRoute(
-  new RegExp('https:.*min.(css|js)'),
+  new RegExp(`https:.*min\.(css|js)`),
   workbox.strategies.staleWhileRevalidate({ cacheName: 'cdn-cache' })
+);
+
+workbox.routing.registerRoute(
+  new RegExp(`.*\.json`),
+  workbox.strategies.networkFirst()
 );
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
